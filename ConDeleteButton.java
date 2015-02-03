f71c0fc640cfa8228c670caf9a8be28adbdb55b9
Joseph Ryan <xazaviar@users.noreply.github.com>
2015-02-03 17:17:14 -0500

    Created file

---
+++ ConDeleteButton.java
@@ -0,0 +1,16 @@
+package Resources;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+
+/**
+ *
+ * @author Xazaviar
+ */
+public class ConDeleteButton extends JButton implements ActionListener{
+
+    public void actionPerformed(ActionEvent e){
+        System.out.println("Make a new .txt");
+    }
+}
