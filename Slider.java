0a407b7f056eb15bea839b6ce1bf118fa75723f6
Joseph Ryan <xazaviar@users.noreply.github.com>
2015-02-03 17:18:07 -0500

    Created File

---
+++ Slider.java
@@ -0,0 +1,19 @@
+
+package Resources;
+
+import javax.swing.JSlider;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+/**
+ *
+ * @author Joseph Ryan
+ */
+public class Slider extends JSlider implements ChangeListener{
+    
+    private int value = 0;
+
+    public void stateChanged(ChangeEvent e) {
+        
+    }
+}
