13b3507a93af12e30252d7f09ea0bb55ef12653c
Joseph Ryan <xazaviar@gmail.com>
2015-03-03 18:16:18 -0500

    Added Module Loading

--- IiSoPGUI/src/Resources/Buttons/StartButton.java
+++ IiSoPGUI/src/Resources/Buttons/StartButton.java
@@ -1,17 +1,47 @@
 
 package Resources.Buttons;
 
+import Resources.Conditional;
+import Tabs.Tab;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 import javax.swing.JButton;
 
 /**
  *
- * @author Xazaviar
+ * @author Joseph Ryan
  */
 public class StartButton extends JButton implements ActionListener{
-
+    
+    private ArrayList<Conditional> conditionals;
+    private int selection = 0;
+    
     public void actionPerformed(ActionEvent e){
-        System.out.println("YOU HAVE FOUND ME");
+        if(selection >-1){
+            File[] files = Tab.finder("Resources/Modules");
+            try {
+                Scanner s = new Scanner(files[selection]);
+                while(s.hasNext()){
+                    String scan = s.nextLine();
+                    for(int l = 0; l < conditionals.size(); l++){
+                        if(scan.equals(conditionals.get(l).getName().substring(0, conditionals.get(l).getName().length()-4))){
+                            conditionals.get(l).toggleEnabled();
+                            break;
+                        }
+                    }
+                }
+                s.close();
+            } catch (FileNotFoundException ex) {}
+        }
+    }
+    
+    
+    public void update(ArrayList<Conditional> conditionals, int selection){
+        this.conditionals = conditionals;
+        this.selection = selection;
     }
 }
--- IiSoPGUI/src/Resources/Conditional.java
+++ IiSoPGUI/src/Resources/Conditional.java
@@ -10,8 +10,6 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
